2cfd6a0598a95fbf724229cacb11a361215132b9
Igor Becker <igor_nyu@hotmail.com>
2022-08-30 22:10:25 -0300

    feat: init studying

--- node/node-init/src/index.ts
+++ node/node-init/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import routes from './routes'
 
 import { settings } from './config';
 
 const app = express();
+
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(settings.port, () =>
   console.log(`Listening on port ${settings.port}`)
