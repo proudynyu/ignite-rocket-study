8d95e70cd62f4965f793089ded3b5ff9d3dfa233
Igor Becker <igor_nyu@hotmail.com>
2022-09-13 22:26:50 -0300

    feat: adding routes

--- next-level-week/nlw-esports/server/src/routes.ts
+++ next-level-week/nlw-esports/server/src/routes.ts
@@ -2,8 +2,22 @@ import { Request, Response, Router } from 'express'
 
 export const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
+router.get('/games', (req: Request, res: Response) => {
+  return res.status(200).json([])
+})
+
+router.post('/ads', (req: Request, res: Response) => {
+  return res.status(201).json([])
+})
+
+router.get('/games/:id/ads', (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: 'Hello there!'
+  })
+})
+
+router.get('/ads/:id/discord', (req: Request, res: Response) => {
+  return res.status(200).json({
     message: 'Hello there!'
   })
 })
