d0b5cfb141c700b4a68708ecf434c2d6edc68c6f
Igor Becker <igor_nyu@hotmail.com>
2023-05-16 19:23:57 -0300

    feat: init server and web

--- next-level-week/nlw-spacetime/server/src/server.ts
+++ next-level-week/nlw-spacetime/server/src/server.ts
@@ -1,13 +1,16 @@
-import Fastify from 'fastify';
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-    return "hello";
-})
+app.get("/users", async () => {
+    const users = await prisma.user.findMany();
+    return users;
+});
 
 app.listen({
     port: 3333
 }).then(() => {
-    console.log('Server is running on port 3333');
-})
+    console.log("Server is running on port 3333");
+});
