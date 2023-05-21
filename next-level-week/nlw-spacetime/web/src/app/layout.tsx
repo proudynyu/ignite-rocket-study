bbfd938651bfc1a51330da27605f7c7a4886b27e
Igor Becker <igor_nyu@hotmail.com>
2023-05-21 14:45:03 -0300

    feat: new page for memories and middleware

--- next-level-week/nlw-spacetime/web/src/app/layout.tsx
+++ next-level-week/nlw-spacetime/web/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import { ReactNode } from "react";
-import "./globals.css";
 import { Roboto_Flex, Bai_Jamjuree } from "next/font/google";
+import { cookies } from "next/headers";
+
+import { Copyright } from "@/components/Copyright";
+import { Hero } from "@/components/Hero";
+import { SignIn } from "@/components/SignIn";
+import { Profile } from "@/components/Profile";
+
+import "./globals.css";
 
 const roboto = Roboto_Flex({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = Bai_Jamjureee({ subsets: ["latin"], weight: '700', variable: '--font-bai-jamjuree' });
@@ -11,9 +18,27 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticate = cookies.has("token");
   return (
     <html lang="en">
-    <body className={`${roboto.variable} ${ baiJamjuree.variable} font-sans bg-gray-900 text-gray-900`}>{children}</body>
+        <body className={`${roboto.variable} ${ baiJamjuree.variable} font-sans bg-gray-900 text-gray-900`}>
+
+            <main className="grid min-h-screen grid-cols-2">
+              <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+                <div className="rounder-full absolute right-0 top-1/2 h-[280px] w-[526px]  -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 blur-full" />
+                <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+                {isAuthenticate ? <Profile /> : <SignIn />}
+                <Hero />
+                <Copyright />
+              </div>
+
+              <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+                {children}
+              </div>
+            </main>
+
+        </body>
     </html>
   );
 }
--- next-level-week/nlw-spacetime/web/src/components/Hero.tsx
+++ next-level-week/nlw-spacetime/web/src/components/Hero.tsx
@@ -1,20 +1,23 @@
 import NextImage from 'next/image'
+import Link from 'next/link'
 
 import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 
 export function Hero() {
     return (
         <div className="space-y-5">
-          <NextImage src={nlwLogo} alt="logo" />
+            <NextImage src={nlwLogo} alt="logo" />
 
-          <div className="max-w-[420px] space-y-4">
-            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">Sua capsula do tempo</h1>
-            <p className="text-lg leading-relaxed">
-            Colecion momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
-            </p>
-          </div>
+            <div className="max-w-[420px] space-y-4">
+                <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">Sua capsula do tempo</h1>
+                <p className="text-lg leading-relaxed">
+                    Colecion momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+                </p>
+            </div>
 
-          <a className="inline-block rounder-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600" href="">CADASTRAR LEMBRANÇA</a>
+            <Link className="inline-block rounder-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600" href="">
+                CADASTRAR LEMBRANÇA
+            </Link>
         </div>
     )
 }
